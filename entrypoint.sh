480c9e00bd849577599248cfcf728863be1e30c8
bstin <barry.github@capsmx.com>
2019-09-10 16:36:18 -0500

    Update entrypoint.sh
    Check to see if keys already exist and prevent regeneration of new keys each time same container is started. This is needed for use with reusable volumes / containers.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
-/usr/libexec/tangd-keygen /var/db/tang
-/usr/libexec/tangd-update /var/db/tang /var/cache/tang
-
+val=$(ls -A /var/db/tang/*.jwk 2>/dev/null | wc -l)
+if [ $val -eq 0 ]
+then
+	echo "Generating new tang keys..."
+	/usr/libexec/tangd-keygen /var/db/tang
+	/usr/libexec/tangd-update /var/db/tang /var/cache/tang
+else
+	echo "Reusing existing tang keys in /var/db/tang"
+fi
 exec "$@"
+
